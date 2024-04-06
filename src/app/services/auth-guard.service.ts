da07fecf49141bba82981a402a71baa2ace7a217
Chakon29 <vi.chacon@duocuc.cl>
2024-04-05 23:26:05 -0300

    Update auth-guard.service.ts

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { environment } from '../../environments/environment';
+
+const poolData = {
+  UserPoolId: environment.cognito.userPoolId,
+  ClientId: environment.cognito.userPoolWebClienId,
+  Region: 'us-east-2'
+};
+
+const userPool = new CognitoUserPool(poolData);
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const cognitoUser = userPool.getCurrentUser();
+    const loggedIn = cognitoUser != null;
+
+    if (!loggedIn) {
+      this.router.navigate(['/login']);
+    }
+
+    return loggedIn;
+  }
 }
