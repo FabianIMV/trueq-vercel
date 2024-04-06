615bd2fd1115d7f2dbf024f5aa75d98e8afb31a6
Chakon29 <vi.chacon@duocuc.cl>
2024-04-05 23:23:54 -0300

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component'; 
-
+import { HOMEComponent } from './home/home.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { OBJETOComponent } from './objeto/objeto.component';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'home', component: HOMEComponent, canActivate: [AuthGuard] },
+  { path: 'objeto', component: OBJETOComponent}
 ];
 
 @NgModule({
