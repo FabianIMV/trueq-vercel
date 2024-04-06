d69346e83cba86444536898d9c1a2d3b171935fd
Chakon29 <vi.chacon@duocuc.cl>
2024-04-05 23:03:09 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    cognito:{
+        userPoolId: 'us-east-2_3ukd5lPHw',
+        userPoolWebClienId: '5hakl714n1fdhulcfect495n2a'
+    }
+}
